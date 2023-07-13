23ae57c1bdb433b4a8a153fa0f96a1bd683ff2dd
Vikram Mali <email@vikrammali.in>
2023-07-13 12:40:23 +0530

    Tracking Link

--- webcomponents/order.js
+++ webcomponents/order.js
@@ -301,7 +301,16 @@ class AppDiv extends CodBellElement {
                                         <li>
                                             <img if="Order.DispatchedOn"  src="/assets/img/image_done.png"/>
                                             <img else src="/assets/img/image_not_done.png"/>
-                                            <span :style="Order.DispatchedOn?'font-weight: bold;':''">Product Dispatched</span>
+                                            <div style="display: flex; flex-direction: column;">
+                                                <span :style="Order.DispatchedOn?'font-weight: bold;':''">Product Dispatched</span>
+                                                <a if="Order.DeliveryLink" target="_blank" :href="Order.DeliveryLink" style="color: #2F8AB2; display: flex; align-items: center;">
+                                                    Tracking Link
+                                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M10.5 13.5L9.45 12.4125L12.1125 9.75H3V8.25H12.1125L9.45 5.5875L10.5 4.5L15 9L10.5 13.5Z" fill="#2F8AB2" fill-opacity="0.6"/>
+                                                    </svg>
+                                                        
+                                                </a>
+                                            </div>
                                         </li>
                                         <li>
                                             <img if="Order.DeliveredOn" src="/assets/img/image_done.png"/>
